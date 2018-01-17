72c46ce8f5720b5824dd853b6c86c6051dd1b0ba
Warley Almeida <almeida.warley@outlook.com>
2018-01-17 19:47:02 +0100

    Add files via upload

--- ABC.py
+++ ABC.py
@@ -4,8 +4,12 @@ import math
 
 '''
 @ Tests:
-
-
+	a) Sphere function: python ABC.py 20 5 2000 100 100 0
+	b) Rastrigin function: python ABC.py 125 10 500 15 50 1
+	c) Griewank function: python ABC.py 125 10 500 600 50 2
+	
+	s1) For ($i=0; $i -le 30; $i++) { python ABC.py 125 10 500 15 50 1 }
+	s2) For ($i=0; $i -le 30; $i++) { python ABC.py 125 10 500 600 50 2 }
 
 @ Parameters:
 	population: 			number of food sources
@@ -15,16 +19,14 @@ import math
 	min_limit:				minimum limit for variables
 	limit: 					limit of iterations without receiving a solution from a scout
 	function:				defines which function will be used as fitness function
-	error:					defines acceptable error between fitness value and optimum value
 '''
 population = 125
 dimension = 10
 max_iteration = 2000
 max_limit = 15
 min_limit = -15
-limit = dimension*population
+limit = 50
 function = 1
-error = 0
 
 class FoodSource:
 	# initializes a food source with a random position
@@ -57,9 +59,7 @@ class FoodSource:
 
 	# calculates the fitness based on a selected function
 	def fitness(self):
-		if self.function == 0:
-			return abs(self.sphere())
-		elif self.function == 1:
+		if self.function == 1:
 			return abs(self.rastrigin())
 		elif self.function == 2:
 			return abs(self.griewank())
@@ -102,10 +102,6 @@ class FoodSource:
 			sum += (-1)*self.values[i]*math.sin(math.sqrt(abs(self.values[i])))
 		return sum
 
-	def rosenbrock(self):
-		return 100*math.pow((self.values[1] - math.pow(self.values[0],2)), 2) + pow((self.values[0] - 1),2)
-
-
 	# explores the neighbourhood of the food source following a specific formula
 	def neighbourhood(self, foodsources):
 		prototype = FoodSource('proto', self.size, self.function)
@@ -138,7 +134,7 @@ def choose(probability):
 	else:
 		return False
 
-'''
+
 # reads arguments
 if len(sys.argv) > 1:
 	population = int(sys.argv[1])
@@ -147,20 +143,22 @@ if len(sys.argv) > 1:
 		if len(sys.argv) > 3: 
 			max_iteration = int(sys.argv[3])
 			if len(sys.argv) > 4:
-				max_limit = int(sys.argv[4])
+				max_limit = abs(int(sys.argv[4]))
 				min_limit = -max_limit
 				if len(sys.argv) > 5:
 					limit = int(sys.argv[5])
+					if len(sys.argv) > 6:
+						function = int(sys.argv[6])
 
-'''
+
 output = open('function' + str(function) + '.txt', 'a')
 foodsources = [FoodSource(str(i), dimension, function) for i in range(population)]
 best_solution = FoodSource('best', dimension, function)
 trials = [0 for i in range(population)]
 n_iteration = 0
 
-# keeps going until the max_iteration is reached or the optimal solution is found
-while best_solution.fitness() > error and n_iteration < max_iteration:
+# keeps going until the max_iteration is reached
+while n_iteration < max_iteration:
 	n_iteration = n_iteration + 1
 
 	# employed bees exploit the neighbourhood of its last position
@@ -177,8 +175,7 @@ while best_solution.fitness() > error and n_iteration < max_iteration:
 	for i in range(population):
 		sumFitness = sumFitness + foodsources[i].fitness()
 	for i in range(population):
-		probability.append(1-float(foodsources[i].fitness())/float(sumFitness))
-
+		probability.append(1 - float(foodsources[i].fitness())/float(sumFitness))
 	# onlookers choose the food sources and exploit them
 	onlookers_index = 0
 	food_index = 0
@@ -198,7 +195,7 @@ while best_solution.fitness() > error and n_iteration < max_iteration:
 	for i in range(1, population):
 		if(trials[i] > trials[max_trials]):
 			max_trials = i
-	if max_trials > limit:
+	if trials[max_trials] > limit:
 		foodsources[max_trials].scout()
 		trials[max_trials] = 0
 
@@ -207,8 +204,6 @@ while best_solution.fitness() > error and n_iteration < max_iteration:
 			best_solution.copy(foodsources[i])
 	print "> Iteration " + str(n_iteration) + " - Best solution: " + str(best_solution.fitness())
 
-#for i in range(population):
-#	foodsources[i].position(str(i))
 print "@ Parameters: \n $ Population size: " + str(population) + "\n $ Dimension: " + str(dimension) + "\n $ Maximum cycle number: " + str(n_iteration) 
 print " $ Maximum limit: " + str(max_limit) + "\n $ Minimum limit: " + str(min_limit) + "\n $ Limit: " + str(limit)
 if function == 1:
@@ -219,16 +214,5 @@ elif function == 3:
 	print " $ Function: Schwefel"
 else:
 	print " $ Function: Sphere"
-print " $ Error: " + str(error)
 best_solution.position('Best solution: ')
 output.write(str(best_solution.fitness())+'\n')
-
-
-population = 125
-dimension = 10
-max_iteration = 2000
-max_limit = 600
-min_limit = -600
-limit = dimension*population
-function = 0
-error = 0
